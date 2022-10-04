4bedea48c5f7e63a64bc1be2bf37556fb6a5ccb8
michaelhill4 <106322946+michaelhill4@users.noreply.github.com>
2022-10-04 18:20:37 -0400

    added handlebars for homepage and dashboard not working

---
+++ controllers/api/commentRoute.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+  
+  router.post('/', withAuth, async (req, res) => {
+   try {
+    const newComment = await Comment.create({
+        ...req.body, 
+        userId: req.session.user_id
+    })
+    res.json(newComment)
+   }
+   catch(err) {
+    res.status(500).json(err)
+   }
+  });
+
+module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,28 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.post('/', async (req, res) => {
+    try {
+      // Find the user who matches the posted e-mail address
+      const newUser = await User.create({
+        email: req.body.email,
+        password: req.body.password
+        });
+  
+      
+      // Create session variables based on the logged in user
+      req.session.save(() => {
+        req.session.user_id = newUser.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: newUser, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
 router.post('/login', async (req, res) => {
     try {
       // Find the user who matches the posted e-mail address
